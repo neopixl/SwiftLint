9ec3ee5aa161bbd31acef19ae5f8f3f8602623fe
Scott Hoyt <scott@wildonion.io>
2016-01-15 08:11:44 -0800

    Increase performance by early return if matches isEmpty.

--- Source/SwiftLintFramework/Extensions/File+SwiftLint.swift
+++ Source/SwiftLintFramework/Extensions/File+SwiftLint.swift
@@ -116,6 +116,9 @@ extension File {
         let contents = self.contents as NSString
         let range = NSRange(location: 0, length: contents.length)
         let matches = matchPattern(pattern, excludingSyntaxKinds: excludingSyntaxKinds)
+        if matches.isEmpty {
+            return []
+        }
         let exclusions = regex(excludingPattern).matchesInString(self.contents,
                                                                  options: [],
                                                                    range: range)
